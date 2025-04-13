fd67bc65fce43f5c40de62f3247feb6502cae8f4
Dawei Hao <dwsamurai84@gmail.com>
2025-04-13 23:25:14 +0800

    Add labels in First VC.

--- Third-Party-Login/FirstViewController.swift
+++ Third-Party-Login/FirstViewController.swift
@@ -10,23 +10,21 @@ import FacebookLogin
 import FacebookCore
 
 class FirstViewController: UIViewController {
-
+    
+    @IBOutlet weak var mainStackView: UIStackView!
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    @IBOutlet weak var givenNameLabel: UILabel!
+    @IBOutlet weak var familyNameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         print("=== FirstViewController ===")
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
